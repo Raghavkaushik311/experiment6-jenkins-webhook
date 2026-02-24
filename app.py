41327e2b8f7899488a1eb0a8306c8345759a5b52
Raghav Kaushik <raghavkaushik311@gmail.com>
2026-02-24 12:21:07 +0530

    Added templates folder with resume

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Experiment 6: Flask app triggered via Jenkins and GitHub Webhook"
+    return "Experiment 6: Jenkins + Templates"
 
-print("Automatic Jenkins build triggered by GitHub push")
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
