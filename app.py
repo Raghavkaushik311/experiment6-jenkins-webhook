ee4552be76cb20bb0a90e3b9a531c5d18dafaee9
Raghav Kaushik <raghavkaushik311@gmail.com>
2026-02-24 09:28:25 +0530

    Final webhook automation test

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def home():
     return "Experiment 6: Flask app triggered via Jenkins and GitHub Webhook"
 
+print("Automatic Jenkins build triggered by GitHub push")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
